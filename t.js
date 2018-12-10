fae90c1ecc778ee1d348ed3bc508d4f0fd350f60
Benjamin Lips <g@mn.tn>
2018-12-10 15:52:31 +0100

    1.5

--- t.js
+++ t.js
@@ -1,6 +1,5 @@
 /*t.js-1.5;(c)2014-2018 - Mntn(r) <https://mn.tn/> c/o Benjamin Lips <g--[AT]--mn.tn>;MIT-Licensed <https://mit-license.org/>;For documentation, see <https://mntn-dev.github.io/t.js/>*/
 
-
 ;(function($){
 
 $.fn.t=function($c,$o){return this.each(function(){
